04e35efb6d3ed64343956367adc2558fd6e46d0b
Gregory <g.parisi1606@gmail.com>
2015-03-09 18:55:43 +0100

    test

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public float angleY = 5.0f; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (0,angleY , 0);
+	
+	}
+}
